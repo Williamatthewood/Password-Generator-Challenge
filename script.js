fb6aa52641aaab0517b0ae11d19637595cffded3
Williamatthewood <w.wood@tcu.edu>
2023-03-11 12:20:20 -0600

    finished functionality

--- script.js
+++ script.js
@@ -41,69 +41,206 @@ function generatePassword(){
 
   if (containsLowercase && containsUppercase && containsNumbers && containsSpecials){
     for (let i = 0; i < passwordLength; i++) {
-      passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
-      // if (passwordArray === []){
-      //   passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
-      // }
-      // else if (lowercase.includes(password[i - 1])){
-      //   passwordArray.push(uppercase[Math.floor(Math.random()* uppercase.length)]);
-      // }
-      // else if (uppercase.includes(password[i - 1])){
-      //   passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
-      // }
-      // else if (numeric.includes(password[i - 1])){
-      //   passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
-      // }
-      // else {
-      //   passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
-      // }
-      
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(uppercase[Math.floor(Math.random()* uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else if (numeric.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
     }
   } 
   else if(containsLowercase && containsUppercase && containsNumbers) {
     console.log("lower, upper, and numbers selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(uppercase[Math.floor(Math.random()* uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsLowercase && containsUppercase && containsSpecials){
     console.log("lower, upper, and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(uppercase[Math.floor(Math.random()* uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsLowercase && containsNumbers && containsSpecials){
     console.log("lower, numbers, and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else if (numeric.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsUppercase && containsNumbers && containsSpecials){
     console.log("upper, numbers, and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else if (numeric.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+    }
   }
   else if (containsLowercase && containsUppercase){
     console.log("lower and upper selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(uppercase[Math.floor(Math.random()* uppercase.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsLowercase && containsNumbers){
     console.log("lower and numbers selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsLowercase && containsSpecials){
     console.log("lower and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+      else if (lowercase.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      }
+    }
   }
   else if (containsUppercase && containsNumbers){
     console.log("upper and numbers selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(numeric[Math.floor(Math.random()* numeric.length)]);
+      }
+      else {
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+    }
   }
   else if(containsUppercase && containsSpecials){
     console.log("upper and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+      else if (uppercase.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      }
+    }
   }
   else if(containsNumbers && containsSpecials){
     console.log("numbers and specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      if (passwordArray.length === 0){
+        passwordArray.push(numeric[Math.floor(Math.random() * numeric.length)]);
+      }
+      else if (numeric.includes(passwordArray[i - 1])){
+        passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      }
+      else {
+        passwordArray.push(numeric[Math.floor(Math.random() * numeric.length)]);
+      }
+    }
   }
   else if(containsLowercase){
     console.log("lower selected");
+    for (let i = 0; i < passwordLength; i++) {
+      passwordArray.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+      
+    }
   }
   else if(containsUppercase){
     console.log("upper selected");
+    for (let i = 0; i < passwordLength; i++) {
+      passwordArray.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+      
+    }
   }
   else if(containsNumbers){
     console.log("numbers selected");
+    for (let i = 0; i < passwordLength; i++) {
+      passwordArray.push(numeric[Math.floor(Math.random() * numeric.length)]);
+    }
   }
   else if(containsSpecials){
     console.log("specials selected");
+    for (let i = 0; i < passwordLength; i++) {
+      passwordArray.push(specialChar[Math.floor(Math.random() * specialChar.length)]);
+      
+    }
   }
   else {
     console.log("nothing was selected");
+    window.alert("You must provide at least 1 password criteria to include!");
+    return undefined;
   }
 
   var password = passwordArray.join("");
